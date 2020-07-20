64fce104d6eb90be24352943405ab71fb286b6c9
jediofgever <fetulahatas1@gmail.com>
2020-07-20 17:22:45 +0800

    download pretrained yolo weights

--- ros_inference.py
+++ ros_inference.py
@@ -81,11 +81,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--image_folder", type=str,
                     default="data/samples", help="path to dataset")
 parser.add_argument("--model_def", type=str, default=relative_path_prefix +
-                    "config/yolov3-custom.cfg", help="path to model definition file")
+                    "config/yolov3.cfg", help="path to model definition file")
 parser.add_argument("--weights_path", type=str, default=relative_path_prefix +
-                    "weights/yolov3_ckpt_98.pth", help="path to weights file")
+                    "weights/yolov3.weights", help="path to weights file")
 parser.add_argument("--class_path", type=str, default=relative_path_prefix +
-                    "data/custom/classes.names", help="path to class label file")
+                    "data/coco.names", help="path to class label file")
 parser.add_argument("--conf_thres", type=float, default=0.8,
                     help="object confidence threshold")
 parser.add_argument("--nms_thres", type=float, default=0.3,
